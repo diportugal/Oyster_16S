56d3d677a0a6aae2f347f70c897b3f28cf10ef1f
diportugal <dportugal8@gmail.com>
2021-06-16 08:38:52 -0700

    Changing the treatment abreviations to the full discriptive name.

--- scripts/2017_metadata_cleaning.R
+++ scripts/2017_metadata_cleaning.R
@@ -1,8 +1,22 @@
-#this will be for the 2017 data set 
+#Oyster 16S Metadata Cleaning 
+#Author: Diana Portugal 
+#Contact: dportugal8@gmail.com 
 
-meta17 <- read.csv("Oyster_data_raw/metadata_de17.csv")
+#This data set has the samples that were sequenced
+
+
+#Loading the data (Original Data is called DE_DATA_ForGenetics_17.csv)
+meta17 <- read.csv("Oyster_data_raw/DE_DATA_ForGenetics_17.csv")
 meta17
 
-#comment 
-#comment 2 
 
+#Renaming the Treatment Names 
+meta17$Treatment
+meta17$Treatment2 <- ifelse(meta17$Treatment =="HH", "HIGH_POLY",
+                            ifelse(meta17$Treatment == "HL", "HIGH_MONO", 
+                                   ifelse(meta17$Treatment == "LL", "LOW_MONO", "LOW_POLY")))
+
+                       
+                          
+                          
+                      
