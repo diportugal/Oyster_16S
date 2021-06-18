9d3ed4a8114774a2664537b8a5053dea1ff825b7
diportugal <dportugal8@gmail.com>
2021-06-18 09:14:15 -0700

    Cleaning the data more

--- scripts/2018_metadata_cleaning.R
+++ scripts/2018_metadata_cleaning.R
@@ -10,27 +10,64 @@
 #MB = Macoma balthica (Clam) NEW NAME is LP - Limecola petalum 
 
 
-#Tasks ####
-#import the 2018 data and change the name of the clams to the new names
-
-
-
-
-#Importing the data (Original Data Name = DE2018_alldata.csv)
-meta18 <- read.csv("Oyster_data_raw/DE2018_alldata.csv")
+#Importing the data (Original Data Name = metadata_de18.csv)
+meta18 <- read.csv("Oyster_data_raw/metadata_de18.csv")
 meta18
 
+#Importing the data (Original Data Name = DE2018_alldata.csv)
+de_data18 <- read.csv("Oyster_data_raw/DE2018_alldata.csv")
+de_data18
 
-#Changing the name to the new name
-meta18$Species2 <- ifelse(meta18$Species =="CV", "CRASSOSTREA_VIRGINICA",
-                            ifelse(meta18$Species == "IR", "ISCHADIUM_RECURVUM", 
-                                   ifelse(meta18$Species == "MM", "AMERITELLA_MITCHELLI", "LIMECOLA_PETALUM")))
+#Importing the data (Original Data Name = asvtable_de18.csv)
+asv18 <- read.csv("Oyster_data_raw/asvtable_de18.csv")
 
 
-#Changing the abbreviations to the new name abbreviation
-meta18$Species_abv_new <- ifelse(meta18$Species =="CV", "CV",
+#Changing the name to the new name (DO NOT USE THIS FOR NOW)
+#meta18$Species2 <- ifelse(meta18$Species =="CV", "CRASSOSTREA_VIRGINICA",
+#ifelse(meta18$Species == "IR", "ISCHADIUM_RECURVUM", 
+#ifelse(meta18$Species == "MM", "AMERITELLA_MITCHELLI", "LIMECOLA_PETALUM")))
+
+
+#Changing the abbreviations to the new name abbreviation (META18)
+meta18$Species <- ifelse(meta18$Species =="CV", "CV",
                           ifelse(meta18$Species == "IR", "IR", 
                                  ifelse(meta18$Species == "MM", "AM", "LP")))
+
+
+#Changing the abbreviations to the new name abbreviation (DE_DATA18)
+de_data18$Species <- ifelse(de_data18$Species =="CV", "CV",
+                                 ifelse(de_data18$Species == "IR", "IR", 
+                                        ifelse(de_data18$Species == "MM", "AM", "LP")))
+
+
+
+
+
+
+
+
+
+
+
+filter(de_data18$Color_Bucket, is.na())
+
+
+#Creating a new column called Bucket_Number
+de_data18$Bucket_Number <- paste0(de_data18$Color_Bucket, de_data18$Number, is.na())
+
+de_data17$Colornumber <- paste0(de_data17$Color, de_data17$Number) 
+
+
+#Creating the UniqueID in DE_DATA18
+
+de_data18$UniqueID <- paste("2018", de_data18$Treatment1_Density, de_data18$Treatment2_Diversity,  )
+
+
+
+
+
+
+
 
 
 
